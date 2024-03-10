d6d7c394ec1df5d0c8d80b532bc9f6397feaed7f
GitGabri <galimero95@hotmail.com>
2024-03-10 16:49:04 +1100

    removed all files to start a static HTML rather than a next JS

--- comps/footer.js
+++
@@ -1,8 +0,0 @@
-import styles from '../styles/home.module.css'
-export default function Footer () {
-	return (
-		 <div className={styles.foot}>
-		 	Copyright 2023 GFM Automation
-		 </div>
-	);
-}
