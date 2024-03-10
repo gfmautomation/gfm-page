e3449263240c493da0bf07b0d1d349ec4fe72668
GitGabri <galimero95@hotmail.com>
2024-03-10 16:51:36 +1100

    starting HTML page. Already have started a home page with a nav bar, title, some content and basic responsiveness. Need to work on navbar when not in mobile mode. At the moment the nav bar is not visible properly, it sits behind the background image, but I want it to be fixed and go away when scrolling down, but come back when scrolling up.

---
+++ hamScript.js
@@ -0,0 +1,9 @@
+const navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu () {
+      navMenu.classList.toggle('menu-open');
+      hamburger.classList.toggle("menu-open");
+};
